91d03f440d92a4d35fd90769dece3b3d9d10abe3
Quinn McHugh <qpmchugh@gmail.com>
2022-07-08 23:02:56 -0400

    Added index.js code to dist/index.js

--- dist/index.js
+++ dist/index.js
@@ -35120,6 +35120,10 @@ const NEO4J_USER = core.getInput("neo4j-user");
 const NEO4J_PASSWORD = core.getInput("neo4j-password");
 const FILENAME = core.getInput("filename");
 const CYPHER_QUERY = core.getInput("cypher-query");
+const CYPHER_RUN_METHOD = core.getInput("cypher-run-method");
+const APOC_ITERATE_CYPHER_ITERATE = core.getInput("apoc-iterate-cypherIterate");
+const APOC_ITERATE_CYPHER_ACTION = core.getInput("apoc-iterate-cypherAction");
+const APOC_ITERATE_BATCH_SIZE = core.getInput("apoc-iterate-batch-size");
 
 const driver = neo4j.driver(
   NEO4J_URI,
@@ -35131,11 +35135,28 @@ const loadData = async () => {
   try {
     const jsonData = JSON.parse(fs.readFileSync(FILENAME));
 
-    const tx = session.beginTransaction();
-
-    const result = await tx.run(CYPHER_QUERY, { value: jsonData });
-    result.records.forEach((record) => console.log(record));
-    await tx.commit();
+    switch (CYPHER_RUN_METHOD) {
+      case "apoc.periodic.iterate": {
+        let query = `CALL apoc.periodic.iterate(
+          '${APOC_ITERATE_CYPHER_ITERATE}',
+          '${APOC_ITERATE_CYPHER_ACTION}',
+          {batchSize: $batchSize, parallel: false, params: {value: $value}}
+          )`;
+        const tx = session.beginTransaction();
+        const result = await tx.run(query, {
+          value: jsonData,
+          batchSize: APOC_ITERATE_BATCH_SIZE,
+        });
+        result.records.forEach((record) => console.log(record));
+        await tx.commit();
+      }
+      case "tx.run": {
+        const tx = session.beginTransaction();
+        const result = await tx.run(CYPHER_QUERY, { value: jsonData });
+        result.records.forEach((record) => console.log(record));
+        await tx.commit();
+      }
+    }
   } catch (error) {
     core.setFailed(error.message);
   } finally {
@@ -35143,8 +35164,6 @@ const loadData = async () => {
     await driver.close();
   }
 };
-
-loadData();
 
 })();
 
